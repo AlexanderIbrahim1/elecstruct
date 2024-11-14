9a8caf2a53a83b8233327776525bd2b2264122f7
Alexander Ibrahim <a68ibrahim@uwaterloo.ca>
2024-11-13 20:47:12 -0500

    Implement nuclear-electron index iterator and create unit tests

--- include/elecstruct/integrals/nuclear_electron_index_iterator.hpp
+++ include/elecstruct/integrals/nuclear_electron_index_iterator.hpp
@@ -22,18 +22,18 @@ class NuclearElectronIndexIterator
 {
 public:
     // clang-format off
-    using iterator_category      = std::forward_iterator_tag;
-    using value_type             = std::tuple<std::int64_t, std::int64_t, std::int64_t>;
-    using size_type              = std::size_t;
-    using difference_type        = std::ptrdiff_t;
-    using pointer                = value_type*;
-    using const_pointer          = const value_type*;
-    using iterator               = pointer;
-    using const_iterator         = const_pointer;
+    using iterator_category = std::forward_iterator_tag;
+    using value_type        = std::tuple<std::int64_t, std::int64_t, std::int64_t>;
+    using size_type         = std::size_t;
+    using difference_type   = std::ptrdiff_t;
+    using pointer           = value_type*;
+    using const_pointer     = const value_type*;
+    using iterator          = pointer;
+    using const_iterator    = const_pointer;
     // clang-format on
 
-    constexpr explicit NuclearElectronIndexIterator(std::int64_t angmom_0, std::int64_t angmom_1)
-        : idx_l_max_ {angmom_0 + angmom_1 + 1}
+    constexpr explicit NuclearElectronIndexIterator(std::int64_t idx_l_max)
+        : idx_l_max_ {idx_l_max}
     {}
 
     constexpr auto operator*() const noexcept -> value_type
@@ -53,20 +53,20 @@ public:
             idx_i_ = 0;
             ++idx_r_;
 
-            idx_i_max_ = update_idx_i_max_(idx_l_, idx_r_);
+            idx_i_max_ = calculate_idx_i_max_(idx_l_, idx_r_);
             if (idx_r_ == idx_r_max_) {
                 idx_r_ = 0;
                 ++idx_l_;
 
-                idx_r_max_ = update_idx_r_max_(idx_l_);
-                idx_i_max_ = update_idx_i_max_(idx_l_, idx_r_);
+                idx_r_max_ = calculate_idx_r_max_(idx_l_);
+                idx_i_max_ = calculate_idx_i_max_(idx_l_, idx_r_);
             }
         }
 
         return *this;
     }
 
-    constexpr auto operator++(int) noexcept -> NuclearElectronIndexIterator&
+    constexpr auto operator++(int) noexcept -> NuclearElectronIndexIterator
     {
         auto temp = *this;
         ++(*this);
@@ -79,8 +79,8 @@ public:
         idx_r_ = 0;
         idx_i_ = 0;
 
-        idx_r_max_ = update_idx_r_max_(idx_l_);
-        idx_i_max_ = update_idx_i_max_(idx_l_, idx_r_);
+        idx_r_max_ = calculate_idx_r_max_(idx_l_);
+        idx_i_max_ = calculate_idx_i_max_(idx_l_, idx_r_);
     }
 
     friend constexpr auto operator==(const NuclearElectronIndexIterator& left, const NuclearElectronIndexIterator& right) noexcept -> bool
@@ -110,15 +110,53 @@ private:
     std::int64_t idx_r_ {0};
     std::int64_t idx_i_ {0};
 
-    constexpr auto update_idx_i_max_(std::int64_t idx_l, std::int64_t idx_r) const noexcept -> std::int64_t
+    constexpr auto calculate_idx_i_max_(std::int64_t idx_l, std::int64_t idx_r) const noexcept -> std::int64_t
     {
         return static_cast<std::int64_t>((idx_l - 2 * idx_r) / 2) + 1;
     }
 
-    constexpr auto update_idx_r_max_(std::int64_t idx_l) const noexcept -> std::int64_t
+    constexpr auto calculate_idx_r_max_(std::int64_t idx_l) const noexcept -> std::int64_t
     {
         return static_cast<std::int64_t>(idx_l / 2) + 1;
     }
+};
+
+
+class NuclearElectronIndices
+{
+public:
+    constexpr explicit NuclearElectronIndices(std::int64_t angmom_0, std::int64_t angmom_1)
+        : idx_l_max_ {angmom_0 + angmom_1 + 1}
+    {}
+
+    constexpr auto begin() noexcept -> NuclearElectronIndexIterator
+    {
+        return NuclearElectronIndexIterator {idx_l_max_};
+    }
+
+    constexpr auto end() noexcept -> NuclearElectronIndexIterator
+    {
+        auto iterator = NuclearElectronIndexIterator {idx_l_max_};
+        iterator.set_to_end();
+
+        return iterator;
+    }
+
+    constexpr auto begin() const noexcept -> NuclearElectronIndexIterator
+    {
+        return NuclearElectronIndexIterator {idx_l_max_};
+    }
+
+    constexpr auto end() const noexcept -> NuclearElectronIndexIterator
+    {
+        auto iterator = NuclearElectronIndexIterator {idx_l_max_};
+        iterator.set_to_end();
+
+        return iterator;
+    }
+
+private:
+    std::int64_t idx_l_max_;
 };
 
 }  // namespace elec
---
+++ test/source/nuclear_electron_index_iterator_test.cpp
@@ -0,0 +1,69 @@
+#include <cstdint>
+#include <tuple>
+#include <vector>
+
+#include <catch2/catch_test_macros.hpp>
+#include <catch2/generators/catch_generators.hpp>
+#include <catch2/matchers/catch_matchers_vector.hpp>
+
+#include "elecstruct/integrals/nuclear_electron_index_iterator.hpp"
+
+
+using Indices = std::tuple<std::int64_t, std::int64_t, std::int64_t>;
+
+
+auto indices_via_standard_nested_loops(std::int64_t angmom_0, std::int64_t angmom_1) -> std::vector<Indices>
+{
+    auto output = std::vector<Indices> {};
+
+    for (std::int64_t idx_l {0}; idx_l < angmom_0 + angmom_1 + 1; ++idx_l)
+    {
+        for (std::int64_t idx_r {0}; idx_r < static_cast<std::int64_t>(idx_l / 2) + 1; ++idx_r)
+        {
+            for (std::int64_t idx_i {0}; idx_i < static_cast<std::int64_t>((idx_l - 2 * idx_r) / 2) + 1; ++idx_i)
+            {
+                output.push_back({idx_l, idx_r, idx_i});
+            }
+        }
+    }
+
+    return output;
+}
+
+
+auto indices_via_custom_iterator(std::int64_t angmom_0, std::int64_t angmom_1) -> std::vector<Indices>
+{
+    auto output = std::vector<Indices> {};
+
+    for (const auto [idx_l, idx_r, idx_i] : elec::NuclearElectronIndices(angmom_0, angmom_1)) {
+        output.push_back({idx_l, idx_r, idx_i});
+    }
+
+    return output;
+}
+
+
+TEST_CASE("nuclear-electron integral index iterator")
+{
+    struct AngularMomentumNumberPairs
+    {
+        std::int64_t angmom_0;
+        std::int64_t angmom_1;
+    };
+
+    auto pair = GENERATE(
+        AngularMomentumNumberPairs {0, 0},
+        AngularMomentumNumberPairs {0, 1},
+        AngularMomentumNumberPairs {1, 0},
+        AngularMomentumNumberPairs {1, 1},
+        AngularMomentumNumberPairs {1, 2},
+        AngularMomentumNumberPairs {1, 3},
+        AngularMomentumNumberPairs {2, 3},
+        AngularMomentumNumberPairs {3, 3}
+    );
+
+    const auto via_loop = indices_via_standard_nested_loops(pair.angmom_0, pair.angmom_1);
+    const auto via_iter = indices_via_custom_iterator(pair.angmom_0, pair.angmom_1);
+
+    REQUIRE_THAT(via_loop, Catch::Matchers::Equals(via_iter));
+}
